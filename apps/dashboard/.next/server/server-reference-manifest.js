3074a73ddc586364027c6b1eed43066a174f79c5
Mj <marciojunior5872@gmail.com>
2026-06-11 23:48:15 -0300

    worker and engine ok

--- apps/dashboard/.next/server/server-reference-manifest.js
+++ apps/dashboard/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"O1GpN/ZDJyRu1wFG6S/f1xg4rqUcWdjUBk7wSNQdVws=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- apps/dashboard/.next/static/chunks/app/jobs/page-d13a758c229d671c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[109],{2723:function(){}},function(n){n.O(0,[227,143,744],function(){return n(n.s=2723)}),_N_E=n.O()}]);
