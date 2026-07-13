ac219c4f9721cff849b9234c93355daf6419696c
Mj <marciojunior5872@gmail.com>
2026-07-13 00:35:19 -0300

    GO ✅

--- packages/scoring/index.ts
+++ packages/scoring/index.ts
@@ -1,10 +1,8 @@
 // packages\scoring\index.tsa
-export * from "./src/scoreEngine";
 export * from "./src/llm/llmEvaluator";
 export * from "./src/preFilter/preFilter.service";
 export * from "./src/preFilter/preFilter.types";
 export * from "./src/llm/llmEvaluator";
-export * from "./src/scoreEngine";
 export * from "./src/llm/llmEvaluator";
 export * from "./src/preFilter/preFilter.service";
 export * from "./src/pipeline/scoring.pipeline";
